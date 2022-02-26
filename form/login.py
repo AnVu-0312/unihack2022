1836e41998bdc93834c5485682a16bc15a9e939a
Binh <hoangthanh_binh8@yahoo.com>
2022-02-26 16:16:20 +1300

    Update login.py

--- form/login.py
+++ form/login.py
@@ -1,8 +1,8 @@
 from PyQt5 import QtWidgets, uic
 import sys
 
-class Ui(QtWidgets.QMainWindow):
+class Login(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
-        uic.loadUi('basic.ui', self)
+        uic.loadUi('../ui/login/login.ui', self)
         self.show()
