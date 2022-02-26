259264f98d2c977589cc20c9551f629c35020e99
baxuan <51146931+baxuan@users.noreply.github.com>
2022-02-26 16:07:56 +1300

    ok

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog,QApplication,QMainWindow
+from PyQt5.QtWidgets import QTableView,QTableWidget,QTableWidgetItem
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.uic import loadUi
+from PyQt5.QtSql import QSqlDatabase, QSqlQueryModel, QSqlQuery
+import sqlite3 
+
 print("test")
